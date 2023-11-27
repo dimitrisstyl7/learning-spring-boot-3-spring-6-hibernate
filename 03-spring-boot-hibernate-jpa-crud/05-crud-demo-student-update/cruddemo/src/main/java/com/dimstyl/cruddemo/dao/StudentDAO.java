48d6157f0aaff173db74678819c38fd4813fed98
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-27 02:12:51 +0200

    '05-crud-demo-student-update' project uploaded.

---
+++ 03-spring-boot-hibernate-jpa-crud/05-crud-demo-student-update/cruddemo/src/main/java/com/dimstyl/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,17 @@
+package com.dimstyl.cruddemo.dao;
+
+import com.dimstyl.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+    void save(Student student);
+
+    Student findById(int id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student student);
+}

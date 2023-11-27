7550300072d5c1d35e395dff4c71991de093e40d
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-27 02:33:49 +0200

    '08-crud-demo-create-db-tables-automatically' project uploaded.

---
+++ 03-spring-boot-hibernate-jpa-crud/08-crud-demo-create-db-tables-automatically/cruddemo/src/main/java/com/dimstyl/cruddemo/dao/impl/StudentDAOImpl.java
@@ -0,0 +1,69 @@
+package com.dimstyl.cruddemo.dao.impl;
+
+import com.dimstyl.cruddemo.dao.StudentDAO;
+import com.dimstyl.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+
+    @Override
+    public Student findById(int id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("from Student", Student.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        return entityManager.createQuery("from Student where lastName = :lastName", Student.class)
+                .setParameter("lastName", lastName)
+                .getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+
+        /*entityManager.createQuery("delete from Student where id = :id")
+                .setParameter("id", id)
+                .executeUpdate();*/
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        return entityManager.createQuery("delete from Student")
+                .executeUpdate();
+    }
+}

83e872022835a00aeb849815e3043f41d2755d17
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-29 18:19:24 +0200

    '04-spring-boot-rest-crud-students-exception-handling' project updated.

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-students-exception-handling/src/main/java/com/dimstyl/demo/rest/StudentRestController.java
@@ -0,0 +1,50 @@
+package com.dimstyl.demo.rest;
+
+import com.dimstyl.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> students;
+
+    // define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Mario", "Doe"));
+        students.add(new Student("John", "Smith"));
+        students.add(new Student("Mary", "White"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // The studentId is the index position of the student in the list
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}

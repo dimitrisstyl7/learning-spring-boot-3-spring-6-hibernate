356fbd942bd4f1ea31a58b7c25823f8740cef1cd
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-29 17:19:53 +0200

    '02-spring-boot-rest-crud-students-list-base' project updated (and renamed to '02-spring-boot-rest-crud-students-list'.

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list/src/main/java/com/dimstyl/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list/src/main/java/com/dimstyl/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.dimstyl.demo.rest;
 
 import com.dimstyl.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-
+    // define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Mario", "Doe"));
         students.add(new Student("John", "Smith"));
         students.add(new Student("Mary", "White"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }

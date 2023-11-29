3b928c3e3ee3fd39f2283b7040aa92b0eb6f4ca7
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-29 17:33:11 +0200

    '03-spring-boot-rest-crud-students-path-variable-get-single-student' project updated.

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-students-path-variable-get-single-student/src/main/java/com/dimstyl/demo/rest/DemoRestController.java
@@ -0,0 +1,13 @@
+package com.dimstyl.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+        @RequestMapping("/hello")
+        public String hello() {
+            return "Hello World!";
+        }
+}

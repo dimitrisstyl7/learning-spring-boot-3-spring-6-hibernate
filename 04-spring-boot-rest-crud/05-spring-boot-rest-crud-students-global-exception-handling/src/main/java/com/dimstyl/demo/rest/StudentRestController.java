fc6a84a7cc07059db1c0bedc9950e4b05e7dd0ae
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-29 19:02:43 +0200

    '05-spring-boot-rest-crud-students-global-exception-handling' project uploaded.

---
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-global-exception-handling/src/main/java/com/dimstyl/demo/rest/StudentRestController.java
@@ -0,0 +1,41 @@
+package com.dimstyl.demo.rest;
+
+import com.dimstyl.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> students;
+
+    // define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Mario", "Doe"));
+        students.add(new Student("John", "Smith"));
+        students.add(new Student("Mary", "White"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        // The studentId is the index position of the student in the list
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return students.get(studentId);
+    }
+}

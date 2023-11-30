8f3bb61c031e7fbd3898b75baaa73b4d87e581e4
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-11-30 23:39:41 +0200

    '24-spring-boot-rest-crud-employee-service-find-add-update-delete' project uploaded.

---
+++ 04-spring-boot-rest-crud/24-spring-boot-rest-crud-employee-service-find-add-update-delete/src/main/java/com/dimstyl/cruddemo/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.dimstyl.cruddemo.service;
+
+import com.dimstyl.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int id);
+}

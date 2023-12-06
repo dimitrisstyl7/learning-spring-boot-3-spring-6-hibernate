90a1d16b9c584c6d5f287f2992341234c73d053a
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-12-06 17:29:52 +0200

    '11-form-data-binding-radio-buttons-read-from-props-file' project uploaded.

---
+++ 06-spring-boot-spring-mvc/11-form-data-binding-radio-buttons-read-from-props-file/src/main/java/com/dimstyl/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,40 @@
+package com.dimstyl.springboot.thymeleafdemo.model;
+
+public class Student {
+    private String firstName, lastName, country, favoriteLanguage;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+}

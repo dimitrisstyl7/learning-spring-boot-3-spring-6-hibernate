3213243d2e879886d6b110349aa7f88830609cd1
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-12-06 16:27:02 +0200

    '08-form-data-binding-dropdown-list-basic' project uploaded.

---
+++ 06-spring-boot-spring-mvc/08-form-data-binding-dropdown-list-basic/src/main/java/com/dimstyl/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,32 @@
+package com.dimstyl.springboot.thymeleafdemo.model;
+
+public class Student {
+    private String firstName, lastName, country;
+
+    public Student() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}

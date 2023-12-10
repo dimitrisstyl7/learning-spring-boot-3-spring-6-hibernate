11193de8d7d3714efcb94084fa7164496d1f6ff7
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-10 13:02:29 +0200

    '00-spring-boot-spring-mvc-crud-starter-code' project uploaded.

---
+++ 07-spring-boot-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/test/java/com/dimstyl/cruddemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimstyl.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

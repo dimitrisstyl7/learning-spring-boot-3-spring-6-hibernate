b54c1fbc7b786410c96f58732eaeec441a61fbdd
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-15 01:40:36 +0200

    '03-jpa-one-to-one-uni-delete-instructor' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-uni-delete-instructor/src/main/java/com/dimstyl/cruddemo/dao/AppDAO.java
@@ -0,0 +1,11 @@
+package com.dimstyl.cruddemo.dao;
+
+import com.dimstyl.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+    Instructor findInstructorById(int id);
+
+    void deleteInstructorById(int id);
+}

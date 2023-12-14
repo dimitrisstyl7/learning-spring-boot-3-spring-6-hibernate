48c4d7f05773d3af11f0e181f4d5eba90b55cdda
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-15 00:48:21 +0200

    '01-jpa-one-to-one-uni-create-instructor' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/dimstyl/cruddemo/CruddemoApplication.java
@@ -0,0 +1,45 @@
+package com.dimstyl.cruddemo;
+
+import com.dimstyl.cruddemo.dao.AppDAO;
+import com.dimstyl.cruddemo.entity.Instructor;
+import com.dimstyl.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+        return runner -> createInstructor(appDAO);
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        /*Instructor instructor =
+                new Instructor("Dim", "Styl", "test@email.com");
+        InstructorDetail instructorDetail =
+                new InstructorDetail("https://github.com/dimitrisstyl7", "coding");
+
+        instructor.setInstructorDetail(instructorDetail);*/
+
+        Instructor instructor =
+                new Instructor("Sam", "Smith", "test@email.com");
+        InstructorDetail instructorDetail =
+                new InstructorDetail("https://youtube.com", "singing");
+
+        instructor.setInstructorDetail(instructorDetail);
+
+        System.out.println("Saving instructor: " + instructor);
+
+        // This will also save the instructorDetail object because of CascadeType.ALL
+        appDAO.save(instructor);
+
+        System.out.println("Instructor saved!");
+    }
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-create-instructor/src/main/java/com/dimstyl/cruddemo/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.dimstyl.cruddemo.dao;
+
+import com.dimstyl.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+}

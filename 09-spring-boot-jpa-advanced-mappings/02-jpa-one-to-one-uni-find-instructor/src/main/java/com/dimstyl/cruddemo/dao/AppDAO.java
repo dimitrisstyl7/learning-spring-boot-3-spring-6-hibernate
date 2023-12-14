9b0a3c0c75667bcbd5944abc6235c470d5784032
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-15 01:02:01 +0200

    '02-jpa-one-to-one-uni-find-instructor' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-uni-find-instructor/src/main/java/com/dimstyl/cruddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.dimstyl.cruddemo.dao;
+
+import com.dimstyl.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+    Instructor findById(int id);
+}

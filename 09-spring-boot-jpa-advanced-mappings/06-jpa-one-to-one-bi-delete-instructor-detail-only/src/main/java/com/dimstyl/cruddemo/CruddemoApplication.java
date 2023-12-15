1c34a6dea5a637e5341e87e56179a079c08bd25f
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-15 02:07:31 +0200

    '06-jpa-one-to-one-bi-delete-instructor-detail-only' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/06-jpa-one-to-one-bi-delete-instructor-detail-only/src/main/java/com/dimstyl/cruddemo/CruddemoApplication.java
@@ -0,0 +1,87 @@
+package com.dimstyl.cruddemo;
+
+import com.dimstyl.cruddemo.dao.AppDAO;
+import com.dimstyl.cruddemo.entity.Instructor;
+import com.dimstyl.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+        return runner -> {
+//            createInstructor(appDAO);
+//            findInstructor(appDAO);
+//            deleteInstructor(appDAO);
+//            findInstructorDetail(appDAO);
+            deleteInstructorDetail(appDAO);
+        };
+    }
+
+    private void deleteInstructorDetail(AppDAO appDAO) {
+        int id = 6;
+        System.out.println("Deleting instructor detail with id: " + id + "...");
+
+        appDAO.deleteInstructorDetailById(id);
+        System.out.println("Instructor detail deleted!");
+    }
+
+    private void findInstructorDetail(AppDAO appDAO) {
+        int id = 1;
+        System.out.println("Finding instructor detail with id: " + id + "...");
+
+        InstructorDetail instructorDetail = appDAO.findInstructorDetailById(id);
+        System.out.println("Found instructor detail: " + instructorDetail);
+
+        System.out.println("Found instructor: " + instructorDetail.getInstructor());
+    }
+
+    private void deleteInstructor(AppDAO appDAO) {
+        int id = 2;
+        System.out.println("Deleting instructor with id: " + id + "...");
+
+        appDAO.deleteInstructorById(id);
+        System.out.println("Instructor deleted!");
+    }
+
+    private void findInstructor(AppDAO appDAO) {
+        int id = 2;
+        System.out.println("Finding instructor with id: " + id + "...");
+
+        Instructor instructor = appDAO.findInstructorById(id);
+        System.out.println("Found instructor: " + instructor);
+
+        System.out.println("Found instructor detail: " + instructor.getInstructorDetail());
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        /*Instructor instructor =
+                new Instructor("Dim", "Styl", "test@email.com");
+        InstructorDetail instructorDetail =
+                new InstructorDetail("https://github.com/dimitrisstyl7", "coding");
+
+        instructor.setInstructorDetail(instructorDetail);*/
+
+        Instructor instructor =
+                new Instructor("Sam", "Smith", "test@email.com");
+        InstructorDetail instructorDetail =
+                new InstructorDetail("https://youtube.com", "singing");
+
+        instructor.setInstructorDetail(instructorDetail);
+
+        System.out.println("Saving instructor: " + instructor);
+
+        // This will also save the instructorDetail object because of CascadeType.ALL
+        appDAO.save(instructor);
+
+        System.out.println("Instructor saved!");
+    }
+}

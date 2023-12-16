612b0ebd59044315f0fce55248ea77a812cfa4b2
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-16 20:56:06 +0200

    '12-jpa-one-to-many-update-course' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/12-jpa-one-to-many-update-course/src/main/java/com/dimstyl/cruddemo/dao/impl/AppDAOImpl.java
@@ -0,0 +1,95 @@
+package com.dimstyl.cruddemo.dao.impl;
+
+import com.dimstyl.cruddemo.dao.AppDAO;
+import com.dimstyl.cruddemo.entity.Course;
+import com.dimstyl.cruddemo.entity.Instructor;
+import com.dimstyl.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+    private final EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        entityManager.remove(findInstructorById(id));
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail instructorDetail = findInstructorDetailById(id);
+
+        // Remove the associated object reference, break bidirectional link
+        instructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(instructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int id) {
+        TypedQuery<Course> query = entityManager.createQuery("from Course where instructor.id = :id", Course.class);
+        query.setParameter("id", id);
+        return query.getResultList();
+    }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int id) {
+        /*TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i join fetch i.courses where i.id = :id", Instructor.class
+        );*/
+        TypedQuery<Instructor> query = entityManager.createQuery(
+                "select i from Instructor i " +
+                        "join fetch i.courses " +
+                        "join fetch i.instructorDetail " +
+                        "where i.id = :id", Instructor.class
+        );
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    @Transactional
+    public void update(Instructor instructor) {
+        entityManager.merge(instructor);
+    }
+
+    @Override
+    public Course findCourseById(int id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void update(Course course) {
+        entityManager.merge(course);
+    }
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/12-jpa-one-to-many-update-course/src/test/java/com/dimstyl/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimstyl.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

99f5886f0f48d569dc57881872637c9d89f85c42
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-16 20:45:57 +0200

    '11-jpa-one-to-many-update-instructor' project uploaded.

---
+++ 09-spring-boot-jpa-advanced-mappings/11-jpa-one-to-many-update-instructor/src/main/java/com/dimstyl/cruddemo/dao/AppDAO.java
@@ -0,0 +1,25 @@
+package com.dimstyl.cruddemo.dao;
+
+import com.dimstyl.cruddemo.entity.Course;
+import com.dimstyl.cruddemo.entity.Instructor;
+import com.dimstyl.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+    Instructor findInstructorById(int id);
+
+    void deleteInstructorById(int id);
+
+    InstructorDetail findInstructorDetailById(int id);
+
+    void deleteInstructorDetailById(int id);
+
+    List<Course> findCoursesByInstructorId(int id);
+
+    Instructor findInstructorByIdJoinFetch(int id);
+
+    void update(Instructor instructor);
+}

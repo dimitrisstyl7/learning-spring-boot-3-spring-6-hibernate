5845185fdf329f5638e4b715384cf05ebf93cb91
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-18 11:53:19 +0200

    '06-spring-boot-aop-pointcut-expressions-match-method-parameter-types' project uploaded.

---
+++ 10-spring-boot-aop/06-spring-boot-aop-pointcut-expressions-match-method-parameter-types/src/main/java/com/dimstyl/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,14 @@
+package com.dimstyl.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+    @Before("execution(* add*(com.dimstyl.aopdemo.Account))")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+    }
+}
---
+++ 10-spring-boot-aop/06-spring-boot-aop-pointcut-expressions-match-method-parameter-types/src/main/java/com/dimstyl/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,7 @@
+package com.dimstyl.aopdemo.dao;
+
+import com.dimstyl.aopdemo.Account;
+
+public interface AccountDAO {
+    void addAccount(Account account);
+}

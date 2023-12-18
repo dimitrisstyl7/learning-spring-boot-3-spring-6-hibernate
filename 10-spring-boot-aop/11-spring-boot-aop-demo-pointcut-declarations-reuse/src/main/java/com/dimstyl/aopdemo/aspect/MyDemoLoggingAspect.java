b19499e6ad164846857db37d7a042e67833276e2
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-19 00:10:39 +0200

    '11-spring-boot-aop-demo-pointcut-declarations-reuse' project uploaded.

---
+++ 10-spring-boot-aop/11-spring-boot-aop-demo-pointcut-declarations-reuse/src/main/java/com/dimstyl/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,24 @@
+package com.dimstyl.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+    @Pointcut("execution(* com.dimstyl.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+    }
+
+    @Before("forDaoPackage()")
+    public void beforeAnyDaoMethodAdvice() {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("=====>>> Performing API analytics");
+    }
+}

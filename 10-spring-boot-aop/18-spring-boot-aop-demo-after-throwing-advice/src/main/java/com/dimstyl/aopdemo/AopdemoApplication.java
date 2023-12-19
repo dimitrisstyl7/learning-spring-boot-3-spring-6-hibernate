0a45048091238ed2f8bc9525bfabc880f72c29fe
dimitrisstyl7 <dimitrisstylianou@proton.me>
2023-12-19 19:37:54 +0200

    Upload '18-spring-boot-aop-demo-after-throwing-advice' project.

---
+++ 10-spring-boot-aop/18-spring-boot-aop-demo-after-throwing-advice/src/main/java/com/dimstyl/aopdemo/AopdemoApplication.java
@@ -0,0 +1,49 @@
+package com.dimstyl.aopdemo;
+
+import com.dimstyl.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopdemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(AccountDAO accountDAO) {
+        return runner -> {
+//            demoTheAfterReturningAdvice(accountDAO);
+            demoTheAfterThrowingAdvice(accountDAO);
+        };
+    }
+
+    private void demoTheAfterThrowingAdvice(AccountDAO accountDAO) {
+        List<Account> accounts = null;
+
+        try {
+            accounts = accountDAO.findAccounts(true);
+
+        } catch (Exception e) {
+            System.out.println("\nMain Program: Caught exception: " + e);
+        }
+
+        System.out.println("\n\nMain Program: AfterThrowingDemoApp");
+        System.out.println("----");
+        System.out.println(accounts);
+        System.out.println("\n");
+    }
+
+    private void demoTheAfterReturningAdvice(AccountDAO accountDAO) {
+        List<Account> accounts = accountDAO.findAccounts();
+        System.out.println("\n\nMain Program: AfterReturningDemoApp");
+        System.out.println("----");
+        System.out.println(accounts);
+        System.out.println("\n");
+    }
+}
---
+++ 10-spring-boot-aop/18-spring-boot-aop-demo-after-throwing-advice/src/main/java/com/dimstyl/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,66 @@
+package com.dimstyl.aopdemo.aspect;
+
+import com.dimstyl.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+    @AfterThrowing(
+            pointcut = "execution(* com.dimstyl.aopdemo.dao.AccountDAO.findAccounts(..))",
+            throwing = "exception"
+    )
+    public void afterThrowingFindAccountsAdvice(JoinPoint joinPoint, Throwable exception) {
+        System.out.println("=====>>> Executing afterThrowingFindAccountsAdvice()");
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("\tMethod signature: " + methodSignature.toShortString());
+        System.out.println("\tException is: " + exception);
+    }
+
+    @AfterReturning(
+            pointcut = "execution(* com.dimstyl.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning = "result"
+    )
+    public void afterReturningFindAccountsAdvice(JoinPoint joinPoint, List<Account> result) {
+        System.out.println("=====>>> Executing afterReturningFindAccountsAdvice()");
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("\tMethod signature: " + methodSignature.toShortString());
+        System.out.println("\tresult is: " + result);
+        convertAccountNamesToUpperCase(result);
+    }
+
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+        result.forEach(account -> account.setName(account.getName().toUpperCase()));
+    }
+
+    @Before("com.dimstyl.aopdemo.aspect.AopExpressionsUtil.forDaoPackageNoGetterSetter()")
+    public void beforeAnyDaoMethodAdvice(JoinPoint joinPoint) {
+        System.out.println("=====>>> Executing beforeAnyDaoMethodAdvice()");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("\tMethod signature: " + methodSignature.toShortString());
+
+        // display method arguments
+        Object[] args = joinPoint.getArgs();
+        Arrays.stream(args).forEach(arg -> {
+            System.out.println("\tMethod argument: " + arg);
+
+            if (arg instanceof Account account) {
+                System.out.println("\tAccount name: " + account.getName());
+                System.out.println("\tAccount level: " + account.getLevel());
+            }
+        });
+    }
+}
